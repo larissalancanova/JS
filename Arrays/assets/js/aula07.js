116eba91e70ccb555998f955cc6b51f69ff7d2b5
larissalancanova <lslancanova@gmail.com>
2023-08-17 17:19:49 -0300

    js com html

---
+++ Arrays/assets/js/aula07.js
@@ -0,0 +1,52 @@
+let formContato = document.getElementById('form-contato');
+let divLista = document.getElementById('lista');
+let nome = [];
+let email = [];
+let telefone = [];
+let assunto = [];
+let mensagem = [];
+
+formContato.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //Atribuindo valores
+    nome.push(formContato.nome.value);
+    email.push(formContato.email.value);
+    telefone.push(formContato.telefone.value);
+    assunto.push(formContato.assunto.value);
+    mensagem.push(formContato.mensagem.value);
+
+    //preencher a div:
+    gerarLista();
+})
+
+function gerarLista() {
+    let auxLista = "";
+
+    for (let posicao = 0; posicao < nome.length; posicao++) {
+        auxLista += `<div class="card">
+                        <div class = "dados-label">
+                            <div>Nome Completo:</div>
+                             <div>${nome[posicao]}</div>
+                        </div>
+                        <div class = "dados-label">
+                            <div>E-mail:</div>
+                            <div>${email[posicao]}</div>
+                        </div>
+                        <div class = "dados-label">
+                            <div>Telefone:</div>
+                            <div>${telefone[posicao]}</div>
+                        </div>
+                        <div class = "dados-label">
+                            <div>Assunto:</div>
+                            <div>${assunto[posicao]}</div>
+                        </div>
+                        <div class = "dados-label">
+                            <div>Mensagem:</div>
+                            <div>${mensagem[posicao]}</div>
+                        </div>
+                    </div>`;
+                    
+    }
+    divLista.innerHTML = auxLista;
+}
